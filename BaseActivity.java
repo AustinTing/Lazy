0abbafb72243a807d307e06f4d80166f5e89a529
CellBody <austintingwork@gmail.com>
2016-09-23 15:16:23 +0800

    Add BaseActivity

---
+++ BaseActivity.java
@@ -0,0 +1,106 @@
+package com.expixel.crazysub;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Created by cellbody on 2016/9/23.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    public ProgressDialog progressDialog;
+    protected static final String TAG = InitApp.TAG;
+
+    private FirebaseAuth auth;
+    private DatabaseReference dbRef;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+
+    public void showProgressDialog(String message) {
+        if (progressDialog == null) {
+            progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage(message);
+            progressDialog.setIndeterminate(true);
+        }
+        progressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (progressDialog != null && progressDialog.isShowing()) {
+            progressDialog.dismiss();
+            Log.i(TAG, this.getClass().getSimpleName() + ": hideProgressDialog");
+
+        }
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, this.getClass().getSimpleName() + ": onCreate");
+
+        auth = FirebaseAuth.getInstance();
+        dbRef = FirebaseDatabase.getInstance().getReference();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, getLocalClassName()+ ": "+"AuthState: signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, getLocalClassName()+ ": "+"AuthState: signed_out");
+                }
+                // ...
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, this.getClass().getSimpleName() + ": onStart");
+
+        auth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, this.getClass().getSimpleName() + ": onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, this.getClass().getSimpleName() + ": onPause");
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG, this.getClass().getSimpleName() + ": onStop");
+//
+        hideProgressDialog();
+        if (mAuthListener != null) {
+            auth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, this.getClass().getSimpleName() + ": onDestroy");
+    }
+
+}
