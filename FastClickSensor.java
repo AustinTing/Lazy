9c991bac3147df444bde9f43474ef54a16d3af7c
CellBody <austintingwork@gmail.com>
2016-09-30 14:21:24 +0800

    Add FastClickSensor

---
+++ FastClickSensor.java
@@ -0,0 +1,18 @@
+package com.expixel.crazysub;
+
+/**
+ * Created by cellbody on 2016/9/30.
+ */
+
+public class FastClickSensor {
+    private static long lastClickTime;
+    public static boolean isFastDoubleClick() {
+        long time = System.currentTimeMillis();
+        long timeD = time - lastClickTime;
+        if ( 0 < timeD && timeD < 500) {
+            return true;
+        }
+        lastClickTime = time;
+        return false;
+    }
+}
